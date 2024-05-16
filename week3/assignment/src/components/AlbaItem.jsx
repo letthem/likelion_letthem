430c40bc709e5e88d338d08e6911d546988bc4d5
letthem <suyeon3662@naver.com>
2024-05-16 22:00:01 +0900

    Design: AlbaItem UI

--- week3/assignment/src/components/AlbaItem.jsx
+++ week3/assignment/src/components/AlbaItem.jsx
@@ -1,12 +1,50 @@
-export default function AlbaItem() {
+import styled from "styled-components";
+
+const Span = styled.span`
+  width: 108px;
+  height: 108px;
+`;
+const Img = styled.img`
+  width: 108px;
+  height: 108px;
+  object-fit: cover;
+  border-radius: 8px;
+`;
+const JobName = styled.div`
+  color: #212124;
+  cursor: pointer;
+  font-size: 18px;
+  line-height: 27.76px;
+  margin-bottom: 4px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  word-break: break-all;
+`;
+const Address = styled.div`
+  color: #868b94;
+  cursor: pointer;
+  font-size: 14px;
+  line-height: 19.6px;
+  margin-bottom: 4px;
+`;
+const Wage = styled.div`
+  color: #212124;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 21.76px;
+`;
+export default function AlbaItem({ imgSrc, jobName, address, wage }) {
   return (
-    <>
-      <div>
-        이미지
-        <h5>문흥 무인아이스크림 매장관리</h5>
-        <h6>단짠 아이스크림</h6>
-        <h5>월급 150,000</h5>
+    <article style={{ display: "flex" }}>
+      <Span>
+        <Img src={imgSrc} />
+      </Span>
+      <div style={{ marginLeft: "16px" }}>
+        <JobName>{jobName}</JobName>
+        <Address>{address}</Address>
+        <Wage>{wage}</Wage>
       </div>
-    </>
+    </article>
   );
 }
